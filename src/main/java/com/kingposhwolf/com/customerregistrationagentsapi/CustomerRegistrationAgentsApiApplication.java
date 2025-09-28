c6f611e7fa209d893332eac4d5e1ccda2dc58b44
kingposhwolf <kingposhwolf@zohomail.com>
2025-09-28 20:51:19 +0300

    Initial project setup with Spring Boot configuration

---
+++ src/main/java/com/kingposhwolf/com/customerregistrationagentsapi/CustomerRegistrationAgentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.kingposhwolf.com.customerregistrationagentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRegistrationAgentsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerRegistrationAgentsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kingposhwolf/com/customerregistrationagentsapi/CustomerRegistrationAgentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingposhwolf.com.customerregistrationagentsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRegistrationAgentsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
